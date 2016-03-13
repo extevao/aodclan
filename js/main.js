9a238de4c3b2bb87a7ad6866ada24694eee8ea26
Estevao <estevaovasques@hotmail.com>
2016-03-13 15:38:06 -0300

    Primeiro commit do site

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function(angular) {
+  'use strict';
+angular.module('aodClan', [])
+  .controller('RecrutamentoController', ['$scope', function($scope) {
+   
+   $scope.submeter = function(user){
+   	if ($scope.formulario.$valid) {
+   		console.log(user);
+   	}
+
+   };
+
+  }]);
+})(window.angular);
